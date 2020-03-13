ef6e622445cdb1ed710baf599c0d75e3c603e669
nongthaihoang <nongthaihoang@gmail.com>
2020-03-13 21:43:02 +0700

    headline option now may work on miui

--- common/install.sh
+++ common/install.sh
@@ -6,25 +6,7 @@ SYSXML=$SYSETC/fonts.xml
 MODPROP=$MODPATH/module.prop
 
 patch() {
-	if [ $PART -eq 1 ]; then
-		sed -i '/\"sans-serif\">/i \
-	<family name="sans-serif">\
-		<font weight="100" style="normal">Thin.ttf</font>\
-		<font weight="100" style="italic">ThinItalic.ttf</font>\
-		<font weight="300" style="normal">Light.ttf</font>\
-		<font weight="300" style="italic">LightItalic.ttf</font>\
-		<font weight="400" style="normal">Regular.ttf</font>\
-		<font weight="400" style="italic">Italic.ttf</font>\
-		<font weight="500" style="normal">Medium.ttf</font>\
-		<font weight="500" style="italic">MediumItalic.ttf</font>\
-		<font weight="900" style="normal">Black.ttf</font>\
-		<font weight="900" style="italic">BlackItalic.ttf</font>\
-		<font weight="700" style="normal">Bold.ttf</font>\
-		<font weight="700" style="italic">BoldItalic.ttf</font>\
-	</family>' $SYSXML
-		sed -i 's/RobotoCondensed/Condensed/' $SYSXML
-	else
-		sed -i '/\"sans-serif\">/i \
+	sed -i '/\"sans-serif\">/i \
 	<family name="sans-serif">\
 		<font weight="100" style="normal">Roboto-Thin.ttf</font>\
 		<font weight="100" style="italic">Roboto-ThinItalic.ttf</font>\
@@ -39,8 +21,11 @@ patch() {
 		<font weight="700" style="normal">Bold.ttf</font>\
 		<font weight="700" style="italic">BoldItalic.ttf</font>\
 	</family>' $SYSXML
-	fi
 	sed -i ':a;N;$!ba; s/ name=\"sans-serif\"//2' $SYSXML
+	if [ $PART -eq 1 ]; then
+		sed -i '/\"sans-serif\">/,/family>/s/Roboto-//' $SYSXML
+		sed -i 's/RobotoCondensed/Condensed/' $SYSXML
+	fi
 }
 
 headline() { cp $FONTDIR/hf/*ttf $SYSFONT; }
@@ -82,26 +67,31 @@ oxygen() {
 }
 
 miui() {
-	if [ $PART -eq 1 ]; then
-		if i=$(grep miui $SYSXML); then
-			sed -i '/\"miui\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
+	if i=$(grep miui $SYSXML); then
+		sed -i '/\"miui\"/,/family>/{/700/,/>/s/MiLanProVF/Bold/;/stylevalue=\"400\"/d}' $SYSXML
+		sed -i '/\"miui-regular\"/,/family>/{/700/,/>/s/MiLanProVF/Medium/;/stylevalue=\"400\"/d}' $SYSXML
+		sed -i '/\"miui-bold\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
+		sed -i '/\"mipro\"/,/family>/{/700/,/>/s/MiLanProVF/Bold/;/stylevalue=\"400\"/d}' $SYSXML
+		sed -i '/\"mipro-regular\"/,/family>/{/700/,/>/s/MiLanProVF/Medium/;/stylevalue=\"400\"/d}' $SYSXML
+		sed -i '/\"mipro-medium\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
+		sed -i '/\"mipro-demibold\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
+		sed -i '/\"mipro-semibold\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
+		sed -i '/\"mipro-bold\"/,/family>/{/400/,/>/s/MiLanProVF/Bold/;/700/,/>/s/MiLanProVF/Black/;/stylevalue/d}' $SYSXML
+		sed -i '/\"mipro-heavy\"/,/family>/{/400/,/>/s/MiLanProVF/Black/;/stylevalue/d}' $SYSXML
+		if [ $PART -eq 1 ]; then
+			sed -i '/\"miui\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/stylevalue=\"340\"/d}' $SYSXML
 			sed -i '/\"miui-thin\"/,/family>/{/400/,/>/s/MiLanProVF/Thin/;/700/,/>/s/MiLanProVF/Light/;/stylevalue/d}' $SYSXML
 			sed -i '/\"miui-light\"/,/family>/{/400/,/>/s/MiLanProVF/Light/;/700/,/>/s/MiLanProVF/Regular/;/stylevalue/d}' $SYSXML
-			sed -i '/\"miui-regular\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/700/,/>/s/MiLanProVF/Medium/;/stylevalue/d}' $SYSXML
-			sed -i '/\"miui-bold\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
-			sed -i '/\"mipro\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
+			sed -i '/\"miui-regular\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/stylevalue=\"340\"/d}' $SYSXML
+			sed -i '/\"mipro\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/stylevalue=\"340\"/d}' $SYSXML
 			sed -i '/\"mipro-thin\"/,/family>/{/400/,/>/s/MiLanProVF/Thin/;/700/,/>/s/MiLanProVF/Light/;/stylevalue/d}' $SYSXML
 			sed -i '/\"mipro-extralight\"/,/family>/{/400/,/>/s/MiLanProVF/Thin/;/700/,/>/s/MiLanProVF/Light/;/stylevalue/d}' $SYSXML
 			sed -i '/\"mipro-light\"/,/family>/{/400/,/>/s/MiLanProVF/Light/;/700/,/>/s/MiLanProVF/Regular/;/stylevalue/d}' $SYSXML
 			sed -i '/\"mipro-normal\"/,/family>/{/400/,/>/s/MiLanProVF/Light/;/700/,/>/s/MiLanProVF/Regular/;/stylevalue/d}' $SYSXML
-			sed -i '/\"mipro-regular\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/700/,/>/s/MiLanProVF/Medium/;/stylevalue/d}' $SYSXML
-			sed -i '/\"mipro-medium\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
-			sed -i '/\"mipro-demibold\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
-			sed -i '/\"mipro-semibold\"/,/family>/{/400/,/>/s/MiLanProVF/Medium/;/700/,/>/s/MiLanProVF/Bold/;/stylevalue/d}' $SYSXML
-			sed -i '/\"mipro-bold\"/,/family>/{/400/,/>/s/MiLanProVF/Bold/;/700/,/>/s/MiLanProVF/Black/;/stylevalue/d}' $SYSXML
-			sed -i '/\"mipro-heavy\"/,/family>/{/400/,/>/s/MiLanProVF/Black/;/stylevalue/d}' $SYSXML
+			sed -i '/\"mipro-regular\"/,/family>/{/400/,/>/s/MiLanProVF/Regular/;/stylevalue=\"340\"/d}' $SYSXML
 			sed -ie 3's/$/-miui&/' $MODPROP
-		fi
+		fi	
+		sed -ie 3's/$/-miui&/' $MODPROP
 	fi
 }
 
